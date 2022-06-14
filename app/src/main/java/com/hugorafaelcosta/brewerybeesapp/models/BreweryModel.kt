22550dffa30d2f18286f28804307191a20d53844
Mariana Barros <marianacb@ciandt.com>
2022-06-14 14:31:43 -0300

    List Layout.

---
+++ app/src/main/java/com/hugorafaelcosta/brewerybeesapp/models/BreweryModel.kt
@@ -0,0 +1,8 @@
+package com.hugorafaelcosta.brewerybeesapp.models
+
+data class BreweryModel(
+    val nome: String,
+    val sizeEvaluations: Double,
+    val breweryType: String,
+    val postalCode: String
+)
